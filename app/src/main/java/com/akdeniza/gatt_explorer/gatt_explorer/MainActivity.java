4319cbc1f6e88037ba864e2dc2c11fafc71820d2
Abdulkerim Akdeniz <abdulkerim.akdeniz@sensorberg.com>
2017-01-02 11:15:02 +0100

    Init android project

---
+++ app/src/main/java/com/akdeniza/gatt_explorer/gatt_explorer/MainActivity.java
@@ -0,0 +1,13 @@
+package com.akdeniza.gatt_explorer.gatt_explorer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
